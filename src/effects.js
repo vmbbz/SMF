c738c7fcd233f9b5ec0ec84c752bb1725e8eb450
vchat-meme-blip <limeikenji@gmail.com>
2026-08-19 16:04:21 +0200

    fix: remove corrupt merge conflict markers; restore files from ab08b19

--- src/effects.js
+++ src/effects.js
@@ -82,11 +82,7 @@ export class Effects {
     }
   }
 
-  addHitParticles(x, y, color = '#ff00ff', count = 25) {
-=======
-  
   addHitParticles(x, y, color = '#0000FF', count = 25) {
->>>>>>> 0c024be (feat: rebrand to MemeFight () and update theme to Base blockchain blue (#0000FF))
     for (let i = 0; i < count; i++) {
       this.particles.push({
         x, y,
